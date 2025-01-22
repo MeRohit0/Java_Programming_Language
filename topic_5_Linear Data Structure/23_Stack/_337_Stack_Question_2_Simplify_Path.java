00e664b70facb5586b00a5d0047bfa00df1ec3b7
Rohit Kumar <rohityadav3461@gmail.com>
2025-01-22 13:14:17 +0530

    adding dsa question

--- topic_5_Linear Data Structure/23_Stack/_337_Stack_Question_2_Simplify_Path.java
+++ topic_5_Linear Data Structure/23_Stack/_337_Stack_Question_2_Simplify_Path.java
@@ -11,20 +11,65 @@
     Sample Input 1 : /a/..
     Sample Output 1 : /
 
+
+
  */ 
 import java.util.Stack;
 public class _337_Stack_Question_2_Simplify_Path {
 
-    public static String simplifyPath(String str){
+    public static void simplifyPath(String str){
         
-        
+        String array[] = str.split("/");
+        //divide the array based on "/"
+        // for(String s : array){
+        // System.out.println(s);} 
+
+        Stack<String> stack = new Stack<String>();
+
+        // String str2 = "/a/b/d/../q/./s/../";
+        /*
+         * a -> b -> d -> b -> q -> s -> q          <--- working 
+         *      a    b    a    b    q    b         <--- stack working
+         *           a         a    b    a
+         *                          a
+         */                         
+
+        for(int i = 0 ; i < array.length ; i++){
+            if(array[i].equals("..")){
+                stack.pop();
+            }else if (array[i].equals(".") || array[i].equals("")){
+                continue;
+            }else{
+                stack.push(array[i]);
+            }
+        }
+
+        Stack<String> revStack = new Stack<String>();
+        while (stack.size() > 0 ) {
+            revStack.push(stack.pop());
+        }
+
+        if(revStack.isEmpty()){
+            System.out.println("/");
+        }else{
+            String output = "/";
+            while(revStack.size() > 0 ){
+                if (revStack.size() != 1 ) {
+                    output = output + revStack.pop() + "/";
+                }else{
+                    output = output + revStack.pop();
+                }
+            }
+            System.out.println(output);
+        }
+
     }
     public static void main(String args[]){
         String str1 = "/apnacollege/";
-        String str2 = "/a/./b/../../c/";
+        String str2 = "/a/b/d/../q/./s/../";
 
-        String str = simplifyPath(str1);
-        System.out.println(str);
+        simplifyPath(str1);
+        simplifyPath(str2);
 
     }
 }
---
+++ topic_5_Linear Data Structure/23_Stack/_339_Stack_Question_4_Trapping_Rain_Water.java
@@ -0,0 +1,18 @@
+/*
+
+   ----------------NOT COMPLETED YET----------------------
+   
+Trapping Rain Water
+We have an array of N non-negative integers arr[] representing an elevation map where the
+width of each bar is 1, compute how much water it is able to trap after raining.
+Note: We have already solved this Question using Arrays but you have to now solve this using
+a Stack
+
+Sample Input 1 : [7 0 4 2 5 0 6 4 0 6]
+Sample Output 1 : 25
+
+*/
+
+public class _339_Stack_Question_4_Trapping_Rain_Water {
+    
+}
